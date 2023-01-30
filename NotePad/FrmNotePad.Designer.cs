c6b685e477a77332bfa93b541c53d40a6735bfb2
HamzaRasheed26 <hamzarasheed19961@gmail.com>
2023-01-30 20:38:27 +0500

    Notepad

---
+++ NotePad/FrmNotePad.Designer.cs
@@ -0,0 +1,373 @@
+﻿namespace NotePad
+{
+    partial class FrmNotePad
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.numSize = new System.Windows.Forms.NumericUpDown();
+            this.lblSize = new System.Windows.Forms.Label();
+            this.cbFontStyle = new System.Windows.Forms.ComboBox();
+            this.lblFont = new System.Windows.Forms.Label();
+            this.btnUnderline = new System.Windows.Forms.Button();
+            this.btnItalic = new System.Windows.Forms.Button();
+            this.btnBold = new System.Windows.Forms.Button();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pasteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.lblWordsNo = new System.Windows.Forms.Label();
+            this.lblLinesNo = new System.Windows.Forms.Label();
+            this.lblWords = new System.Windows.Forms.Label();
+            this.lblLines = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.richTxtMain = new System.Windows.Forms.RichTextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numSize)).BeginInit();
+            this.menuStrip.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.numSize);
+            this.panel1.Controls.Add(this.lblSize);
+            this.panel1.Controls.Add(this.cbFontStyle);
+            this.panel1.Controls.Add(this.lblFont);
+            this.panel1.Controls.Add(this.btnUnderline);
+            this.panel1.Controls.Add(this.btnItalic);
+            this.panel1.Controls.Add(this.btnBold);
+            this.panel1.Controls.Add(this.menuStrip);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(807, 55);
+            this.panel1.TabIndex = 0;
+            // 
+            // numSize
+            // 
+            this.numSize.Location = new System.Drawing.Point(355, 26);
+            this.numSize.Maximum = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            this.numSize.Name = "numSize";
+            this.numSize.Size = new System.Drawing.Size(43, 23);
+            this.numSize.TabIndex = 5;
+            this.numSize.Value = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.numSize.ValueChanged += new System.EventHandler(this.numSize_ValueChanged);
+            // 
+            // lblSize
+            // 
+            this.lblSize.AutoSize = true;
+            this.lblSize.Location = new System.Drawing.Point(322, 31);
+            this.lblSize.Name = "lblSize";
+            this.lblSize.Size = new System.Drawing.Size(27, 15);
+            this.lblSize.TabIndex = 4;
+            this.lblSize.Text = "Size";
+            // 
+            // cbFontStyle
+            // 
+            this.cbFontStyle.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cbFontStyle.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cbFontStyle.FormattingEnabled = true;
+            this.cbFontStyle.Location = new System.Drawing.Point(159, 26);
+            this.cbFontStyle.Name = "cbFontStyle";
+            this.cbFontStyle.Size = new System.Drawing.Size(133, 23);
+            this.cbFontStyle.TabIndex = 3;
+            this.cbFontStyle.SelectedIndexChanged += new System.EventHandler(this.cbFontStyle_SelectedIndexChanged);
+            // 
+            // lblFont
+            // 
+            this.lblFont.AutoSize = true;
+            this.lblFont.Location = new System.Drawing.Point(122, 31);
+            this.lblFont.Name = "lblFont";
+            this.lblFont.Size = new System.Drawing.Size(31, 15);
+            this.lblFont.TabIndex = 2;
+            this.lblFont.Text = "Font";
+            // 
+            // btnUnderline
+            // 
+            this.btnUnderline.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnUnderline.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUnderline.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
+            this.btnUnderline.Location = new System.Drawing.Point(68, 26);
+            this.btnUnderline.Name = "btnUnderline";
+            this.btnUnderline.Size = new System.Drawing.Size(22, 23);
+            this.btnUnderline.TabIndex = 1;
+            this.btnUnderline.Text = "U";
+            this.btnUnderline.UseVisualStyleBackColor = false;
+            this.btnUnderline.Click += new System.EventHandler(this.btnUnderline_Click);
+            // 
+            // btnItalic
+            // 
+            this.btnItalic.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnItalic.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnItalic.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.btnItalic.Location = new System.Drawing.Point(40, 26);
+            this.btnItalic.Name = "btnItalic";
+            this.btnItalic.Size = new System.Drawing.Size(22, 23);
+            this.btnItalic.TabIndex = 1;
+            this.btnItalic.Text = "I";
+            this.btnItalic.UseVisualStyleBackColor = false;
+            this.btnItalic.Click += new System.EventHandler(this.btnItalic_Click);
+            // 
+            // btnBold
+            // 
+            this.btnBold.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnBold.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBold.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnBold.Location = new System.Drawing.Point(12, 26);
+            this.btnBold.Name = "btnBold";
+            this.btnBold.Size = new System.Drawing.Size(22, 23);
+            this.btnBold.TabIndex = 1;
+            this.btnBold.Text = "B";
+            this.btnBold.UseVisualStyleBackColor = false;
+            this.btnBold.Click += new System.EventHandler(this.btnBold_Click);
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(807, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip1";
+            this.menuStrip.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip_ItemClicked);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.newToolStripMenuItem.Text = "New";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem,
+            this.pasteToolStripMenuItem,
+            this.cutToolStripMenuItem,
+            this.deleteToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.copyToolStripMenuItem.Text = "Copy";
+            // 
+            // pasteToolStripMenuItem
+            // 
+            this.pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
+            this.pasteToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.pasteToolStripMenuItem.Text = "Paste";
+            // 
+            // cutToolStripMenuItem
+            // 
+            this.cutToolStripMenuItem.Name = "cutToolStripMenuItem";
+            this.cutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.cutToolStripMenuItem.Text = "Cut";
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 474);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(807, 30);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.lblWordsNo);
+            this.panel4.Controls.Add(this.lblLinesNo);
+            this.panel4.Controls.Add(this.lblWords);
+            this.panel4.Controls.Add(this.lblLines);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(213, 30);
+            this.panel4.TabIndex = 0;
+            // 
+            // lblWordsNo
+            // 
+            this.lblWordsNo.AutoSize = true;
+            this.lblWordsNo.Location = new System.Drawing.Point(162, 6);
+            this.lblWordsNo.Name = "lblWordsNo";
+            this.lblWordsNo.Size = new System.Drawing.Size(12, 15);
+            this.lblWordsNo.TabIndex = 2;
+            this.lblWordsNo.Text = "-";
+            // 
+            // lblLinesNo
+            // 
+            this.lblLinesNo.AutoSize = true;
+            this.lblLinesNo.Location = new System.Drawing.Point(52, 6);
+            this.lblLinesNo.Name = "lblLinesNo";
+            this.lblLinesNo.Size = new System.Drawing.Size(12, 15);
+            this.lblLinesNo.TabIndex = 2;
+            this.lblLinesNo.Text = "-";
+            // 
+            // lblWords
+            // 
+            this.lblWords.AutoSize = true;
+            this.lblWords.Location = new System.Drawing.Point(116, 6);
+            this.lblWords.Name = "lblWords";
+            this.lblWords.Size = new System.Drawing.Size(47, 15);
+            this.lblWords.TabIndex = 1;
+            this.lblWords.Text = "Words :";
+            // 
+            // lblLines
+            // 
+            this.lblLines.AutoSize = true;
+            this.lblLines.Location = new System.Drawing.Point(12, 6);
+            this.lblLines.Name = "lblLines";
+            this.lblLines.Size = new System.Drawing.Size(40, 15);
+            this.lblLines.TabIndex = 1;
+            this.lblLines.Text = "Lines :";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.richTxtMain);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 55);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(807, 419);
+            this.panel3.TabIndex = 1;
+            // 
+            // richTxtMain
+            // 
+            this.richTxtMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTxtMain.Location = new System.Drawing.Point(0, 0);
+            this.richTxtMain.Name = "richTxtMain";
+            this.richTxtMain.Size = new System.Drawing.Size(807, 419);
+            this.richTxtMain.TabIndex = 0;
+            this.richTxtMain.Text = "";
+            this.richTxtMain.TextChanged += new System.EventHandler(this.richTxtMain_TextChanged);
+            // 
+            // FrmNotePad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 504);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "FrmNotePad";
+            this.Text = "NotePad";
+            this.Load += new System.EventHandler(this.FrmNotePad_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numSize)).EndInit();
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private MenuStrip menuStrip;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private Button btnUnderline;
+        private Button btnItalic;
+        private Button btnBold;
+        private ToolStripMenuItem newToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripMenuItem editToolStripMenuItem;
+        private ToolStripMenuItem copyToolStripMenuItem;
+        private ToolStripMenuItem pasteToolStripMenuItem;
+        private ToolStripMenuItem cutToolStripMenuItem;
+        private ToolStripMenuItem deleteToolStripMenuItem;
+        private Panel panel4;
+        private Label lblWords;
+        private Label lblLines;
+        private RichTextBox richTxtMain;
+        private Label lblSize;
+        private ComboBox cbFontStyle;
+        private Label lblFont;
+        private Label lblWordsNo;
+        private Label lblLinesNo;
+        private NumericUpDown numSize;
+    }
+}
